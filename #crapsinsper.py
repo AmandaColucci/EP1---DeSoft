64801e5a15ed2c8925edb55d42fb53c7a24b9a4b
AmandaColucci <62797435+AmandaColucci@users.noreply.github.com>
2020-04-02 19:03:49 -0300

    adicionei o field

--- #crapsinsper.py
+++ #crapsinsper.py
@@ -5,6 +5,5 @@ nome = input("Qual o seu nome? ")
 print("Olá, {0}!".format(nome))
  
 saldo = 1000
- 
 print("{0}, o seu saldo é de {1} fichas".format(nome,saldo))
 print("Você está na fase Come out")
